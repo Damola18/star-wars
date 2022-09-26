d189f2fa1354db4b7d3286c7ecce0bca9f283eab
Damola Olutoke <olutokeezekiel18@gmail.com>
2022-09-26 22:28:45 +0100

    First commit

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 * UI Framework - [MUI](https://https://mui.com/ "")
 * Fonts - [Google-Fonts](https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap/ "")
 
+## Usage
+Click [here](https://damola18.github.io/star-wars/ "here") to open the page in your browser
+
--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import React from 'react';
 import Container from './components/Container/container';
 import Tables from './components/Table/table';
 
 function App() {
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 12000);
-  }, []);
   return (
    <div>
         <Container/>
